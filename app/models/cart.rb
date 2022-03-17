2730e5ea391f4e406b6ebcef959d2b2cd5adf70f
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2022-03-17 14:51:24 -0300

    removing validation

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,3 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  validates :name, :phone, :address, :scheduled_at, presence: true
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -4,11 +4,4 @@ RSpec.describe Cart, type: :model do
   describe 'associations' do
     it { should have_many(:cart_items).class_name('CartItem') }
   end
-
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:phone) }
-    it { should validate_presence_of(:address) }
-    it { should validate_presence_of(:scheduled_at) }
-  end
 end
