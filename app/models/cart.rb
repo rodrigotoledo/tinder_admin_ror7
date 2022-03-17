7fd53c15216cd2ac5658d9c20a1b5153b7bdc614
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2022-03-17 15:18:32 -0300

    broadcast to carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
+
+  after_create do
+    broadcast_append_to "carts", target: "carts", partial: "carts/cart", locals: {cart: self}
+  end
+  after_update do
+    broadcast_update_to "carts", partial: "carts/cart", locals: {cart: self}
+  end
+  after_destroy_commit do
+    broadcast_remove_to "carts", target: "cart_#{self.id}"
+  end
 end
